fc41a276dbc829dc0856b06b7e1e042d8c3fbea2
Linniel Driver-Williams <LinnielDW@googlemail.com>
2026-04-03 21:52:16 +0200

    added extra info to todo

--- ShadowfallCode/Singletons/CargoDrawManager.cs
+++ ShadowfallCode/Singletons/CargoDrawManager.cs
@@ -1,5 +1,6 @@
 ﻿//TODO: readd when singletonmodels are properly supported (and remove CombatManagerStartTurnPatch).
 // see https://discord.com/channels/309399445785673728/1478917653551906947/1487458524790264070 for more details
+// see https://github.com/Alchyr/BaseLib-StS2/commit/677306eb3af5353d0ec073c5c5842b43168bcda9 for implementation details
 
 // using MegaCrit.Sts2.Core.Commands;
 // using MegaCrit.Sts2.Core.Entities.Cards;
