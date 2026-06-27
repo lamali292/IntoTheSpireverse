b75edf60c591eff2cef798fcf8248649194a2f12
LinnielDW <linnieldw@gmail.com>
2026-06-27 21:38:55 +0200

    implemented TrashCompactor's onplay

--- IntoTheSpireverseCode/Cards/ShadowRegent/TrashCompactor.cs
+++ IntoTheSpireverseCode/Cards/ShadowRegent/TrashCompactor.cs
@@ -1,7 +1,10 @@
+using IntoTheSpireverse.IntoTheSpireverseCode.CardPiles;
+using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace IntoTheSpireverse.IntoTheSpireverseCode.Cards.ShadowRegent;
@@ -20,12 +23,26 @@ public class TrashCompactor() : ShadowRegentCard(0,
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
-        CardPlay play)
+        CardPlay cardPlay)
     {
-        for (var i = 0; i < ResolveEnergyXValue(); i++)
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .WithHitCount(ResolveEnergyXValue())
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_giant_horizontal_slash", null, "slash_attack.mp3")
+            .Execute(choiceContext);
+
+        var cargoPile = CargoCardPile.CargoPileType.GetPile(Owner)
+            .Cards.OrderBy(c => c.Rarity)
+            .ThenBy(c => c.Id).ToList();
+        var cardSelectorPrefs = new CardSelectorPrefs(SelectionScreenPrompt, 0, ResolveEnergyXValue());
+        var selection = (await CardSelectCmd.FromSimpleGrid(choiceContext, cargoPile, Owner, cardSelectorPrefs));
+
+        foreach (var cardModel in selection)
         {
-            
-            
+            await CardCmd.Exhaust(choiceContext, cardModel);
         }
     }
 
