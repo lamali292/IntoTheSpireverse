eeff1597ee92f3dbe15b3a143eb7c34447736b5d
LinnielDW <linnieldw@gmail.com>
2026-06-27 22:27:33 +0200

    finished implementing Breach

--- IntoTheSpireverseCode/Cards/ShadowRegent/Breach.cs
+++ IntoTheSpireverseCode/Cards/ShadowRegent/Breach.cs
@@ -1,7 +1,10 @@
+using IntoTheSpireverse.IntoTheSpireverseCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace IntoTheSpireverse.IntoTheSpireverseCode.Cards.ShadowRegent;
@@ -11,29 +14,60 @@ public class Breach() : ShadowRegentCard(2,
     CardRarity.Rare,
     TargetType.AnyEnemy)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new DamageVar(18, ValueProp.Move)
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(18, ValueProp.Move),
+        new IntVar("Increase", 2)
     ];
-    
+
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
-        CardPlay play)
+        CardPlay cardPlay)
     {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
         await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
             .FromCard(this)
-            .Targeting(play.Target)
+            .Targeting(cardPlay.Target)
             .WithHitFx("vfx/vfx_attack_blunt", null, "blunt_attack.mp3")
             .Execute(choiceContext);
-        
-        DynamicVars.Damage.BaseValue += DynamicVars["Increase"].BaseValue;
 
-        //TODO - Temp one-time Chem X power.
+        await PowerCmd.Apply<BreachPower>(
+            new ThrowingPlayerChoiceContext(),
+            Owner.Creature,
+            DynamicVars["Increase"].BaseValue,
+            Owner.Creature,
+            this);
     }
-    
-   
+
     protected override void OnUpgrade()
     {
         DynamicVars.Damage.UpgradeValueBy(6);
+    }
+}
+
+public class BreachPower : ShadowPowerModel
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override int ModifyXValue(CardModel card, int originalValue)
+    {
+        if (Owner != card.Owner.Creature)
+        {
+            return originalValue;
+        }
+
+        return originalValue + Amount;
+    }
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Card.EnergyCost.CostsX && cardPlay.Card.Owner.Creature == Owner)
+        {
+            Flash();
+            await PowerCmd.Remove(this);
+        }
     }
 }
